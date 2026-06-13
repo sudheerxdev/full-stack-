2fae0bd87bc736d2b72bf2eca6cb62fdc4b1174c
sudheerxdev <sudheer01yadav2005@gmail.com>
2026-06-13 23:36:08 +0530

    javascript revision

---
+++ javascript/index.js
@@ -0,0 +1,12 @@
+// function message(){
+//     console.log("Hello, World!");
+// }
+// message();
+ // function without a return and without a parameter
+
+ function message(){
+    return "hello sudheer";
+ }
+let msg =  message();
+console.log(msg);
+ // function without parameter but with a return
