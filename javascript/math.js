1a57cd4d8dc375f7be45a461e050b04c7bef2c63
sudheerxdev <sudheer01yadav2005@gmail.com>
2026-06-27 14:06:06 +0530

    completed math revision in js

---
+++ javascript/math.js
@@ -0,0 +1,30 @@
+// const score = 100 ; 
+// console.log(score) ;
+// console.log(typeof score) ;
+
+// // Math object in JavaScript
+// console.log(Math);
+//  console.log(score.toString().length);
+//  console.log(score.toString().toFixed(2));
+
+// let num = 23.98888;
+// console.log(num.toPrecision(4));
+
+// const hundreades = 10000000;
+// console.log(hundreades.toLocaleString('en-IN'));
+// // Math object in JavaScript is a built-in object that provides properties and methods for mathematical constants and functions. It is not a function object. The Math object has properties and methods that can be used to perform mathematical operations on numbers.
+
+
+// math in js 
+console.log(Math.PI);
+console.log(Math.E);
+console.log(Math.SQRT2);
+console.log(Math.abs(-5));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.8));
+console.log(Math.round(4.5));
+console.log(Math.max(1, 2, 3, 4, 5));
+console.log(Math.min(1, 2, 3, 4, 5));
+console.log(Math.pow(2, 3));
+console.log(Math.sqrt(16));
+console.log(Math.random()*100);
